5bd027142a6d6df9b3726dacab4371d4c61cb860
Georg Semmler <georg_semmler_05@web.de>
2015-08-25 14:41:16 +0200

    Fix snapshot url
    Change-Id: Ic37a42a588834457633839bd7cf235ad5d2a4b66

--- project.clj
+++ project.clj
@@ -7,5 +7,5 @@
                  [org.clojure/java.jdbc "0.4.1"]
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]
                  ]
-  :repositories [["snapshots" {:url "https://maven.azapps.de/artifactory/mirakeldb-snapshot"}]
+  :repositories [["snapshots" {:url "https://maven.azapps.de/artifactory/mirakeldb-snapshots"}]
                  ["releases" {:url "https://maven.azapps.de/artifactory/mirakeldb-releases"}]])
